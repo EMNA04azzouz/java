5d87cf18567ed2bafe20f4c6dabc794c6d657bfa
EMNA04azzouz <131978684+EMNA04azzouz@users.noreply.github.com>
2024-09-23 19:26:50 +0100

    Add files via upload

---
+++ prosit1 - Copie/src/Main_2.java
@@ -0,0 +1,11 @@
+public class Main_2 {
+    public static void main(String[] args) {
+
+        Animal lion = new Animal("felines","lion",14,true);
+        Zoo myZoo = new Zoo("Zoonmame",25,"tunis");
+        
+        lion.afficher();
+        myZoo.afficher();
+        
+    }
+}
